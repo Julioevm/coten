35409e8fad8d0dac295aa929a9e4c56d54f5afd0
Julio Valls <julioevm@gmail.com>
2023-12-08 09:36:08 +0100

    Refactor entity generation to include a entity_factory file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
+import copy
 import tcod
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
 from input_handlers import EventHandler
 
 
 def main() -> None:
+    """Main startup function."""
     screen_width = 80
     screen_height = 50
 
@@ -26,7 +28,7 @@ def main() -> None:
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    player = copy.deepcopy(entity_factories.player)
 
     game_map = generate_dungeon(
         max_rooms=max_rooms,
--- procgen.py
+++ procgen.py
@@ -5,6 +5,7 @@ from typing import Iterator, List, Tuple, TYPE_CHECKING
 
 import tcod
 
+import entity_factories
 from game_map import GameMap
 import tile_types
 
@@ -74,9 +75,9 @@ def place_entities(
 
         if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
             if random.random() < 0.8:
-                pass  # TODO: Place an Orc here
+                entity_factories.orc.spawn(dungeon, x, y)
             else:
-                pass  # TODO: Place a Troll here
+                entity_factories.troll.spawn(dungeon, x, y)
 
 
 def tunnel_between(
