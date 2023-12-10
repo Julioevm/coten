9f7b2acaaa00a12d7080d51380df5e09c3417b03
Julio Valls <julioevm@gmail.com>
2023-12-10 15:36:16 +0100

    Add game saving function

--- engine.py
+++ engine.py
@@ -9,6 +9,8 @@ import exceptions
 from message_log import MessageLog
 from render_functions import render_bar, render_names_at_mouse_location
 
+import lzma
+import pickle
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
@@ -53,3 +55,9 @@ class Engine:
         )
 
         render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
