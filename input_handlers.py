b36b343fd5dd753038a50113258b2311ac36415e
Julio Valls <julioevm@gmail.com>
2023-12-16 10:02:30 +0100

    Update GameOver screen.
    Now you can restart without quititng.

--- input_handlers.py
+++ input_handlers.py
@@ -577,18 +577,54 @@ class MainGameEventHandler(EventHandler):
 
 
 class GameOverEventHandler(EventHandler):
+    def on_render(self, console: tcod.Console) -> None:
+        console.tiles_rgb["bg"] = color.black
+        console.tiles_rgb["fg"] = color.red
+        console.print(
+            console.width // 2,
+            console.height // 2,
+            "YOU DIED!",
+            fg=color.red,
+            alignment=tcod.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 1,
+            "Prees any key to continue",
+            fg=color.red,
+            alignment=tcod.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 2,
+            "Prees Esc to exit",
+            fg=color.red,
+            alignment=tcod.CENTER,
+        )
+
     def on_quit(self) -> None:
         """Handle exiting out of a finished game."""
         if os.path.exists("savegame.sav"):
             os.remove("savegame.sav")  # Deletes the active save file.
         raise exceptions.QuitWithoutSaving()  # Avoid saving a finished game.
 
+    def on_restart(self) -> MainGameEventHandler:
+        """Handle restarting the game."""
+        from setup_game import new_game  # pylint: disable=import-outside-toplevel
+
+        if os.path.exists("savegame.sav"):
+            os.remove("savegame.sav")  # Deletes the active save file.
+
+        return MainGameEventHandler(new_game())
+
     def ev_quit(self, event: tcod.event.Quit) -> None:
         self.on_quit()
 
-    def ev_keydown(self, event: tcod.event.KeyDown) -> None:
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[MainGameEventHandler]:
         if event.sym == tcod.event.KeySym.ESCAPE:
             self.on_quit()
+        else:
+            return self.on_restart()
 
 
 class HistoryViewer(EventHandler):
