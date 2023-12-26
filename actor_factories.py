5fdb60daa4e0a1dc315eb50f4bae5cae9a4bdf05
Julio Valls <julioevm@gmail.com>
2023-12-26 10:14:14 +0100

    Tweak some enemy values.

--- actor_factories.py
+++ actor_factories.py
@@ -35,7 +35,7 @@ brute_zombie = Actor(
     name="Brute Zombie",
     ai_cls=HostileEnemy,
     equipment=Equipment(),
-    fighter=Fighter(hp=20, base_defense=2, base_power=4),
+    fighter=Fighter(hp=16, base_defense=2, base_power=2),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=80),
 )
@@ -57,7 +57,7 @@ ghoul = Actor(
     name="Ghoul",
     ai_cls=HostileEnemy,
     equipment=Equipment(),
-    fighter=Fighter(hp=16, base_defense=1, base_power=4),
+    fighter=Fighter(hp=14, base_defense=1, base_power=4),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=75),
 )
@@ -68,7 +68,7 @@ werewolf = Actor(
     name="Werewolf",
     ai_cls=HostileEnemy,
     equipment=Equipment(),
-    fighter=Fighter(hp=30, base_defense=2, base_power=6),
+    fighter=Fighter(hp=18, base_defense=2, base_power=6),
     inventory=Inventory(capacity=0),
-    level=Level(xp_given=300),
+    level=Level(xp_given=160),
 )
