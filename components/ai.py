23a82594fe3f3ca2522e961738e67a4e889ca2d9
Julio Valls <julioevm@gmail.com>
2024-01-01 11:23:33 +0100

    Make staticRangedEnemy simply shoot in place.

--- components/ai.py
+++ components/ai.py
@@ -151,22 +151,8 @@ class StaticRangedEnemy(BaseAI):
 
     def perform(self) -> None:
         target = self.engine.player
-        dx = target.x - self.entity.x
-        dy = target.y - self.entity.y
-        distance = max(abs(dx), abs(dy))  # Chebyshev distance.
 
         if self.engine.game_map.visible[self.entity.x, self.entity.y]:
-            if distance > 1:
-                return RangedAttackAction(self.entity, (target.x, target.y)).perform()
-
-            self.path = self.get_path_to(target.x, target.y)
-
-        if self.path:
-            dest_x, dest_y = self.path.pop(0)
-            return MovementAction(
-                self.entity,
-                dest_x - self.entity.x,
-                dest_y - self.entity.y,
-            ).perform()
+            return RangedAttackAction(self.entity, (target.x, target.y)).perform()
 
         return WaitAction(self.entity).perform()
