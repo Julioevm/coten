d0a35fda4b8a7e8fd66ce3bf2fdffb1c53e7e27e
Julio Valls <julioevm@gmail.com>
2024-01-04 15:19:17 +0100

    Create turn manager class.

---
+++ turn_manager.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import heapq
+
+if TYPE_CHECKING:
+    from entity import Actor
+
+
+class TurnManager:
+    def __init__(self):
+        self.queue = []
+
+    @property
+    def has_actors(self):
+        return len(self.queue) > 0
+
+    def add_actor(self, actor: Actor):
+        heapq.heappush(self.queue, (-actor.fighter.energy, actor.id, actor))
+
+    def get_next_actor(self) -> Actor | None:
+        if self.queue:
+            _, _, actor = heapq.heappop(self.queue)
+            return actor
+        return None
