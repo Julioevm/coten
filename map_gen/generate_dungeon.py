e155ae9326e202771b96c1f83ebf944e7265051e
Julio Valls <julioevm@gmail.com>
2024-01-16 09:00:47 +0100

    Use inner_bounds method instead of the deprecated inner property.

--- map_gen/generate_dungeon.py
+++ map_gen/generate_dungeon.py
@@ -62,28 +62,23 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             has_placed_first_door = False
             has_placed_second_door = False
+
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 tile = dungeon.tiles[x, y]
-
-                # From the start of the tunnel, the first non walkable tile is the wall we want
-                # to place the door on. But we have to check if there already
-                # are empty spaces around it.
-                inner_area = rooms[-1].inner
-                is_within_x_bounds = inner_area[0].start < x < inner_area[0].stop
-                is_within_y_bounds = inner_area[1].start < y < inner_area[1].stop
-
-                is_within_bounds = is_within_x_bounds and is_within_y_bounds
+                is_within_bounds = rooms[-1].is_within_inner_bounds(x, y)
                 walkable_count = len(list(dungeon.get_walkable_adjacent_tiles(x, y)))
 
                 if not is_within_bounds and not has_placed_first_door:
-                    # check if the tile is surrounded by no more than 3 walking tiles
+                    # check if the tile is surrounded by no more than 4 walking tiles
                     if walkable_count <= 4:
                         dungeon.tiles[x, y] = tile_types.closed_door
                         has_placed_first_door = True
                         continue
 
                 # when reaching the boundaries of the new_room add a door
-                if (new_room.is_within_bounds(x, y)) and not has_placed_second_door:
+                if (
+                    new_room.is_within_inner_bounds(x, y)
+                ) and not has_placed_second_door:
                     if walkable_count <= 4:
                         dungeon.tiles[x, y] = tile_types.closed_door
                         has_placed_second_door = True
