bb7978f574adb284cebbf330fc6d1b21e595580f
Julio Valls <julioevm@gmail.com>
2024-01-18 09:12:49 +0100

    Add event handler for Victory screen.

--- event_handlers/game_over_event_handler.py
+++ event_handlers/game_over_event_handler.py
@@ -28,14 +28,14 @@ class GameOverEventHandler(EventHandler):
         )
         console.print(
             console.width // 2,
-            console.height // 2 + 1,
-            "Press any key to restart",
+            console.height // 2 + 3,
+            "Press Enter to restart",
             fg=color.red,
             alignment=libtcodpy.CENTER,
         )
         console.print(
             console.width // 2,
-            console.height // 2 + 2,
+            console.height // 2 + 4,
             "Press Esc to exit",
             fg=color.red,
             alignment=libtcodpy.CENTER,
@@ -62,5 +62,78 @@ class GameOverEventHandler(EventHandler):
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[MainGameEventHandler]:
         if event.sym == tcod.event.KeySym.ESCAPE:
             self.on_quit()
-        else:
+        elif (
+            event.sym == tcod.event.KeySym.KP_ENTER
+            or event.sym == tcod.event.KeySym.RETURN
+        ):
+            return self.on_restart()
+
+
+class VictoryEventHandler(EventHandler):
+    """Event handler for the game over screen."""
+
+    def on_render(self, console: tcod.console.Console) -> None:
+        console.rgb["bg"] = color.black
+        console.rgb["fg"] = color.red
+        console.print(
+            console.width // 2,
+            console.height // 2,
+            "Congratulations!",
+            fg=color.yellow,
+            alignment=libtcodpy.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 1,
+            "You defeated the evil in the castle!",
+            fg=color.yellow,
+            alignment=libtcodpy.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 3,
+            f"Turns: {self.engine.current_turn} Total Exp: {self.engine.player.level.total_xp}",
+            fg=color.red,
+            alignment=libtcodpy.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 5,
+            "Press Enter to start a new game",
+            fg=color.red,
+            alignment=libtcodpy.CENTER,
+        )
+        console.print(
+            console.width // 2,
+            console.height // 2 + 6,
+            "Press Esc to exit",
+            fg=color.red,
+            alignment=libtcodpy.CENTER,
+        )
+
+    def on_quit(self) -> None:
+        """Handle exiting out of a finished game."""
+        if os.path.exists("savegame.sav"):
+            os.remove("savegame.sav")  # Deletes the active save file.
+        raise exceptions.QuitWithoutSaving()  # Avoid saving a finished game.
+
+    def on_restart(self) -> MainGameEventHandler:
+        """Handle restarting the game."""
+        from setup_game import new_game  # pylint: disable=import-outside-toplevel
+
+        if os.path.exists("savegame.sav"):
+            os.remove("savegame.sav")  # Deletes the active save file.
+
+        return MainGameEventHandler(new_game())
+
+    def ev_quit(self, event: tcod.event.Quit) -> None:
+        self.on_quit()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[MainGameEventHandler]:
+        if event.sym == tcod.event.KeySym.ESCAPE:
+            self.on_quit()
+        elif (
+            event.sym == tcod.event.KeySym.KP_ENTER
+            or event.sym == tcod.event.KeySym.RETURN
+        ):
             return self.on_restart()
