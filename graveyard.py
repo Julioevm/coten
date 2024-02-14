344287b43ce62fa3fce6a4627307b3de1b331219
Julio Valls <julioevm@gmail.com>
2024-02-14 16:51:40 +0100

    Add a simple graveyard system.

---
+++ graveyard.py
@@ -0,0 +1,32 @@
+import datetime
+import os
+
+from engine import Engine
+from entity import Actor
+
+GRAVERYARD_FILE = "graveyard.txt"
+
+
+def check_graveyard_file_exists():
+    root_dir = os.getcwd()  # Get the current working directory
+    graveyard_file = os.path.join(
+        root_dir, GRAVERYARD_FILE
+    )  # Assuming the file name is "graveyard.txt"
+
+    if os.path.isfile(graveyard_file):
+        return True
+    else:
+        return False
+
+
+def create_graveyard_entry(player: Actor, engine: Engine) -> None:
+    root_dir = os.getcwd()  # Get the current working directory
+    graveyard_file = os.path.join(
+        root_dir, GRAVERYARD_FILE
+    )  # Assuming the file name is "graveyard.txt"
+
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(graveyard_file, "a") as file:
+        file.write(
+            f"+ Player Exp: {player.level.total_xp} Turns: {engine.current_turn} Died at floor {engine.game_world.current_floor} on {current_date} +\n"
+        )
