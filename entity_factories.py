a00ce2be545117595ebff0a3ac8bc07ef94a8244
Julio Valls <julioevm@gmail.com>
2024-02-14 11:53:38 +0100

    Add more decorative entities.

--- entity_factories.py
+++ entity_factories.py
@@ -10,3 +10,9 @@ grave = Entity(char="#", color=(78, 50, 13), name="grave", blocks_movement=False
 bones = Entity(char="%", color=(225, 215, 235), name="bones", blocks_movement=False)
 
 shrine = Entity(char="+", color=(255, 215, 0), name="shrine", blocks_movement=True)
+
+torch = Entity(char="t", color=(255, 147, 41), name="torch", blocks_movement=False)
+
+statue = Entity(char="S", color=(192, 192, 192), name="statue", blocks_movement=True)
+
+fountain = Entity(char="F", color=(0, 191, 255), name="fountain", blocks_movement=False)
